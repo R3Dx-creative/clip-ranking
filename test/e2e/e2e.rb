3fd409fb365dfc2c16df78a1997a3b9208c1f987
conf8o <41929738+conf8o@users.noreply.github.com>
2021-06-22 18:31:58 +0900

    e2e の提案修正

--- test/e2e/e2e.rb
+++ test/e2e/e2e.rb
@@ -4,31 +4,27 @@ require_relative '../../app/lib/clip'
 
 class EndtoEndTest < Test::Unit::TestCase
   def test_e2e
-    # Queueに移動
-    # POST: /enqueue
-    clips = Clip.clips("0.Shared")
-    clips.concat(Clip.clips("2.Revenging"))
-    for clip in clips
-      clip.move!("1.Queue", :force)
-    end
-    ClipClassifier::Hisroty.save(clips, :queue)
-
-    # クリップの通知
-    # POST: /message?folder=[folder]
-    folder = "1.Queue"
-    ClipMessanger.send_view_links(folder)
-
-    # ランキング集計
-    # GET: /aggregate?date=[date]
-    date = ClipClassifier::History.last_date
-    result = ClipAggregator.aggregate(date)
+    # クリップの通知とQueueへの移動
+    # POST: /notify
+    GoogleDriveUtils.move("0.Shared", "1.Queue")
+    videos = GoogleDriveUtils.videos("1.Queue").map{ |video| 
+      { id: video.id, name: video.name, link: video.web_view_link }
+    }
+    videos.each(DiscordUtils.post)
+    JsonUtils.save("shared.json", videos)
 
     # 仕分け
-    # POST: /classify, bodyにresult
+    # POST: /classify
+    clip_hash = JsonUtils.load("shared.json").map { |video| [video.id, video.name] }.to_h
+    result = DiscordUtils.history(100).filter_map { |message| 
+      id = google_drive_file_id(message.content)
+      reaction_size = message.reactions.size
+      [clip_hash[id], reaction_size] if id && clip_pool.include?(id)
+    }.to_h
     clips = Clip.clips("1.Queue", result)
     sorted = ClipClassifier.classify(clips)
     sorted.foreach(&:commit!)
-    ClipClassifier::History.save(sorted, :claasified)
+    ClipClassifier::History.save(sorted)
 
     # クリップの結合
     # POST: editor/join?folder=[folder]
@@ -41,5 +37,17 @@ class EndtoEndTest < Test::Unit::TestCase
     date = ClipClassifier::History.last_date
     ranked_folder = format(ClipClassifier::RANKED_FOLDER, date) + "/joined.mp4"
     ClipEditor.post(ranked_folder)
+  end
+
+  def test_e2e2
+    folder = "0.Shared"
+    shared = Shared.new(folder)
+    shared.notify
+
+    folder = "1.Queue"
+    queue = Queue.merge(folder, shared)
+
+    clips = Queue.load
+    ClipClassifier.claasify(clips)
   end
 end
