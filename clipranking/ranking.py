549e769bbda192a289fb1711c181f9ed5754b824
conf8o <41929738+conf8o@users.noreply.github.com>
2021-07-29 11:14:17 +0900

    例外追加

--- clipranking/ranking.py
+++ clipranking/ranking.py
@@ -9,19 +9,27 @@ class Ranking:
     クリップ仕分け機。仕分け元のディレクトリ名と仕分け先、仕分ける対象のフィルタ条件を保持する。
     """
 
-    def __init__(self,
-                 filter_key: Callable[[Clip], bool],
-                 sort_key: Callable[[Clip], SupportsLessThan],
-                 rules: dict[str, Callable[[int, Clip], bool]]):
+    class NonExhaustivePatterns(Exception):
+        pass
+
+    def __init__(
+        self,
+        filter_key: Callable[[Clip], bool],
+        sort_key: Callable[[Clip], SupportsLessThan],
+        rules: dict[str, Callable[[int, Clip], bool]]
+    ) -> None:
         self.filter_key = filter_key
         self.sort_key = sort_key
         self.rules = rules
 
     def ranked(self, clips: Iterable[Clip]) -> Iterator[Clip]:
         for i, clip in enumerate(sorted(filter(self.filter_key, clips), key=self.sort_key)):
-            clip.dest = next(dir
-                for dir, condition
-                in self.rules.items()
-                if condition(i, clip)
-            )
-            yield clip
+            try:
+                clip.dest = next(dir
+                    for dir, condition
+                    in self.rules.items()
+                    if condition(i, clip)
+                )
+                yield clip
+            except StopIteration:
+                raise Ranking.NonExhaustivePatterns(clip)
